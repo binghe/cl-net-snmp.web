98169b27c150b02d73062ae87e32e57568d323a1
Chun Tian (binghe) <binghe.lisp@gmail.com>
2016-10-26 20:59:41 +0200

    removed ".git" directories from packages ...

--- release/release.sh
+++ release/release.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -x
 
+rm -rf $1-$2/.git
+rm -f $1-$2.tar.gz*
 tar cvzf $1-$2.tar.gz $1-$2
-rm -rf $1-$2
 rm $1_latest.tar.gz
 ln -s $1-$2.tar.gz $1_latest.tar.gz
 gpg -b -a $1-$2.tar.gz
